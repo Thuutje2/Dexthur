5afc28ebaf8dde42893d5eb5bf2e58747b71b280
Thuutje <thuutje2@gmail.com>
2024-10-13 00:38:54 +0200

    cooldown change

--- cooldown.js
+++ cooldown.js
@@ -1,9 +1,13 @@
 const cooldown = 24 * 60 * 60 * 1000; // 24 hours in milliseconds
+const hour = 60 * 60 * 1000; // 1 hour in milliseconds
 
 function getCooldownTime(lastGuessDate) {
     const currentTime = new Date();
     const nextAvailableGuessDate = new Date(lastGuessDate);
-    nextAvailableGuessDate.setHours(24, 0, 0, 0); // Zet de tijd op middernacht van de volgende dag
+    // uur voor volgende gok
+    nextAvailableGuessDate.setTime(nextAvailableGuessDate.getTime() + hour);
+
+    // nextAvailableGuessDate.setHours(24, 0, 0, 0); // Zet de tijd op middernacht van de volgende dag
 
     const timeDifference = nextAvailableGuessDate - currentTime;
     const timeRemaining = Math.max(timeDifference, 0); // Zorg ervoor dat we geen negatieve waarden krijgen
@@ -16,4 +20,4 @@ function getCooldownTime(lastGuessDate) {
     return { remainingHours: 0, remainingMinutes: 0, timeRemaining: 0 };
 }
 
-module.exports = { getCooldownTime, cooldown };
+module.exports = { getCooldownTime, hour };
