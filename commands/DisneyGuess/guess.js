45840e4ac2945bb35f05695dde0b5e2c829e3e70
Thuutje <thuutje2@gmail.com>
2024-10-13 12:41:01 +0200

    Updated guess

--- commands/DisneyGuess/guess.js
+++ commands/DisneyGuess/guess.js
@@ -23,13 +23,8 @@ module.exports = {
 
             const currentTime = new Date();
 
-            if (userGuessData) {
-                const cooldownData = getCooldownTime(userGuessData.last_correct_guess_date);
-
-                if (cooldownData.timeRemaining > 0) {
-                    return message.reply(`You have to wait until ${cooldownData.remainingMinutes} minutes before you can guess again.`);
-                }
-            } else {
+            // Handle initial setup of user data
+            if (!userGuessData) {
                 await query('INSERT INTO User_Points (user_id, username, points, last_guess_date, last_correct_guess_date, streak, daily_character_id, failed_attempts, hints_given) VALUES ($1, $2, 0, null, null, 0, null, 0, 0)', [message.author.id, message.author.username]);
                 userGuessData = {
                     failed_attempts: 0,
@@ -39,27 +34,54 @@ module.exports = {
                 };
             }
 
-            // Check if it's time to set a new character
-            if (!userGuessData.daily_character_id || (currentTime - new Date(userGuessData.last_guess_date) > fifteenMinutes)) {
+            // Check cooldown for the current character
+            if (userGuessData.daily_character_id) {
+                if (userGuessData.last_correct_guess_date) {
+                    const nextAvailableGuessDate = new Date(userGuessData.last_correct_guess_date);
+                    nextAvailableGuessDate.setTime(nextAvailableGuessDate.getTime() + fifteenMinutes);
+                    if (currentTime < nextAvailableGuessDate) {
+                        const cooldownData = getCooldownTime(userGuessData.last_correct_guess_date);
+                        return message.reply(`You have to wait until ${cooldownData.remainingMinutes} minutes before you can guess again.`);
+                    }
+                }
+
+                // If all hints are used, inform user
+                if (userGuessData.failed_attempts >= 6) {
+                    return message.reply(`You've used all hints. The character was **${dailyCharacter.name}**. Please wait for 15 minutes before guessing again.`);
+                }
+            } else {
+                // Fetch a new character if none is currently being guessed
                 const dailyCharacterResult = await query('SELECT * FROM disney_characters ORDER BY RANDOM() LIMIT 1');
                 const dailyCharacter = dailyCharacterResult.rows[0];
 
                 await query('UPDATE User_Points SET daily_character_id = $1, last_guess_date = $2, failed_attempts = 0, hints_given = 0 WHERE user_id = $3', [dailyCharacter.id, currentTime, message.author.id]);
-
                 userGuessData.daily_character_id = dailyCharacter.id;
                 userGuessData.failed_attempts = 0;
                 userGuessData.hints_given = 0;
             }
 
+            // Fetch the current character
             const characterResult = await query('SELECT * FROM disney_characters WHERE id = $1', [userGuessData.daily_character_id]);
             const dailyCharacter = characterResult.rows[0];
+            const dailyCharacterHints = dailyCharacter.hints;
 
+            // Check if the guessed character is correct
             if (guessedCharacter === dailyCharacter.name.toLowerCase()) {
                 const streak = userGuessData.streak + 1;
-                const pointsEarned = 50 - (userGuessData.failed_attempts * 10);
-                const newPoints = (userGuessData.points || 0) + pointsEarned;
 
-                await query('UPDATE User_Points SET last_guess_date = $1, last_correct_guess_date = $2, streak = $3, points = $4, failed_attempts = 0, daily_character_id = null WHERE user_id = $5', [currentTime, currentTime, streak, newPoints, message.author.id]);
+                let pointsEarned = 0;
+                switch (userGuessData.failed_attempts) {
+                    case 0: pointsEarned = 50; break;
+                    case 1: pointsEarned = 40; break;
+                    case 2: pointsEarned = 30; break;
+                    case 3: pointsEarned = 20; break;
+                    case 4: pointsEarned = 10; break;
+                    case 5: pointsEarned = 5; break;
+                    default: pointsEarned = 0; break;
+                }
+
+                const newPoints = (userGuessData.points || 0) + pointsEarned;
+                await query('UPDATE User_Points SET last_guess_date = $1, last_correct_guess_date = $2, streak = $3, points = $4, failed_attempts = 0 WHERE user_id = $5', [currentTime, currentTime, streak, newPoints, message.author.id]);
 
                 const embed = new EmbedBuilder()
                     .setTitle('Correct Guess!')
@@ -70,20 +92,24 @@ module.exports = {
                 message.channel.send({ embeds: [embed] });
             } else {
                 const failedAttempts = userGuessData.failed_attempts + 1;
+                const hintsGiven = Math.min(failedAttempts, dailyCharacterHints.length);
 
-                await query('UPDATE User_Points SET failed_attempts = $1 WHERE user_id = $2', [failedAttempts, message.author.id]);
+                await query('UPDATE User_Points SET failed_attempts = $1, hints_given = $2 WHERE user_id = $3', [failedAttempts, hintsGiven, message.author.id]);
 
                 if (failedAttempts < 6) {
                     const embed = new EmbedBuilder()
-                        .setTitle('Incorrect Guess!')
-                        .setDescription(`The guess "${args.join(' ')}" was incorrect. You will be able to guess again in 15 minutes.`)
-                        .setColor(0xc56af0);
+                        .setTitle('Guess the Disney Character')
+                        .setDescription('Guess the character!')
+                        .setColor(0xc56af0)
+                        .addFields({ name: 'Hints', value: dailyCharacterHints.slice(0, hintsGiven).join('\n') });
 
                     message.channel.send({ embeds: [embed] });
                 } else {
                     await query('UPDATE User_Points SET daily_character_id = null, streak = 0 WHERE user_id = $1', [message.author.id]);
 
-                    return message.reply(`Unfortunately, you've run out of attempts. The character was **${dailyCharacter.name}**. You can guess again in 15 minutes.`);
+                    const cooldownData = getCooldownTime(currentTime);
+
+                    return message.reply(`Unfortunately, you have run out of hints. The character was **${dailyCharacter.name}**. You will be able to guess again in ${cooldownData.remainingMinutes} minutes.`);
                 }
             }
 
@@ -92,8 +118,8 @@ module.exports = {
             message.channel.send('An error occurred while guessing the character. Please try again later.');
         }
     }
-};
 
+};
 
 
 
